91c7f74581f49556a788dcda0a0c46a6e6e2bcdb
Morten Wynde <morten.wynde@gmail.com>
2026-03-27 12:54:19 +0000

    Stylised UI and added main menu

--- studyTracker/src/main/java/com/example/studytracker/LoginController.java
+++
@@ -1,7 +0,0 @@
-package com.example.studytracker;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class LoginController {
-}
---
+++ studyTracker/src/main/java/com/example/studytracker/startScreenController.java
@@ -0,0 +1,35 @@
+package com.example.studytracker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class startScreenController {
+
+    @FXML
+    //login button gets pressed - opens main menu window
+    private void loginPressed(ActionEvent event) throws IOException {
+        /*
+        TO ADD: proper login system that checks username and password
+         */
+
+
+        //getting new file
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainMenu.fxml"));
+        Parent root = fxmlLoader.load();
+
+        //getting current stage
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        //displaying new scene
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
