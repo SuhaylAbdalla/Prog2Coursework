1b0d2446b082366bea7fa239821e8883682d9600
lulo166 <146172440+lulo166@users.noreply.github.com>
2026-03-31 09:21:32 +0000

    more database tests

--- Database/database_init.java
+++ Database/database_init.java
@@ -1,7 +1,16 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class database_init {
     public static void main(String[] args) {
-        String url = "jdbc:sqlite:user.db";
+    Connection conn = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/",
+        "username",
+        "password"
+    );
+
+    Statement stmt = conn.createStatement();
+    stmt.executeUpdate("CREATE DATABASE user");
     }
 }
