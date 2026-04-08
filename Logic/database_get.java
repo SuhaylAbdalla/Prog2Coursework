b7cfbfb1a0bcaa49647fa341351070b0f78231f5
SuhaylAbdalla <suhayl.abdalla@gmail.com>
2026-04-08 12:30:15 +0100

    Add files via upload
    Framework for querying database

---
+++ Logic/database_get.java
@@ -0,0 +1,21 @@
+
+public class database_get {
+    
+    public String[] getTopLocations() { // gets the locations most frequently studied at
+        String[] topLocations = {};
+        return topLocations;
+    }
+
+    public String[] getProductiveTimes() { // gets the times most frequently studied at 
+        String[] productiveTimes = {};
+        return productiveTimes;
+    }
+
+    public double getAverageFocusTime(String timeframe) { // gets the average focus time of study sessions in a given timeframe
+        return 0.0;
+    }
+
+    public double getStudyHours(String timeframe) { // gets the number of hours studied in a given timeframe
+        return 0.0;
+    }
+}
