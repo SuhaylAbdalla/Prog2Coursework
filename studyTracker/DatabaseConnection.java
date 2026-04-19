67c2b1779951b5d1805ffe72c60c0c84b4468bf3
lulo166 <146172440+lulo166@users.noreply.github.com>
2026-04-19 17:46:21 +0000

    trying to fix db

--- studyTracker/DatabaseConnection.java
+++ studyTracker/DatabaseConnection.java
@@ -47,13 +47,10 @@ public class DatabaseConnection {
         
         String create_user = "CREATE TABLE users (id_user INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL UNIQUE, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL);";
         
-        
         String create_course = "CREATE TABLE courses (id_course INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, course_name TEXT NOT NULL, course_code TEXT NOT NULL);";
-        
         
         String create_module = "CREATE TABLE modules (id_module INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, module_name TEXT NOT NULL, module_code TEXT NOT NULL, module_course_id INTEGER NOT NULL, FOREIGN KEY (module_course_id) REFERENCES courses(id_course));";
         
-        //assignments include exams as well !!
         String create_assignment = "CREATE TABLE assignments (id_assignment INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, assignment_title TEXT NOT NULL, assignment_deadline DATETIME, assignment_weight FLOAT, grade_received FLOAT, assignment_module_id INTEGER NOT NULL, FOREIGN KEY (assignment_module_id) REFERENCES modules(id_module));";
         
         String create_study_session = "CREATE TABLE study_sessions (id_session INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, start DATETIME NOT NULL, end DATETIME, duration TIME, location Text, productivity INTEGER NOT NULL CHECK(productivity >= 0 AND productivity <11), session_assignment_id INTEGER NOT NULL, FOREIGN KEY (session_assignment_id) REFERENCES assignments(id_assignment));";
@@ -76,5 +73,7 @@ public class DatabaseConnection {
             System.err.println("Error when initialising the database : " + e.getMessage());
         }
         closeDatabaseConnection();
+
+        
     } 
 }
