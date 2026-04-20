2a5147166dbe33f5480445c165ca428af1c84e22
lulo166 <146172440+lulo166@users.noreply.github.com>
2026-04-20 11:30:43 +0000

    test launcher

---
+++ Database/NewLauncher.java
@@ -0,0 +1,27 @@
+package com.example.studytracker;
+
+import javafx.application.Application;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//launches program
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+        try {
+            ProcessBuilder pb = new ProcessBuilder("python", "script.py");
+            Process process = pb.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
